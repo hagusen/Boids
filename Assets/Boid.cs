7ff980e7b143d2615f277556d48af1f1aea24466
hagusen <john@bit-ab.com>
2020-10-09 18:16:52 +0200

    start

---
+++ Assets/Boid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    public Vector2 acceleration;
+    public Vector2 velocity;
+
+    void Start()
+    {
+        
+    }
+
+
+
+    void Update()
+    {
+        transform.Translate(velocity * Time.deltaTime);
+
+
+
+
+    }
+}
